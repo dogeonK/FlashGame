a445f844d678c4136a9d57bfb9f0f7daba89b2ca
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-09-02 21:30:06 +0900

    [UPDATE] 주석 추가, gameList, 랜덤 아이디 생성

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 let tabs = document.querySelectorAll('.task-tabs div');
-let mode = 'all';
+let gameList = [];
 let filterList = [];
+let mode = 'all';
 let underLine = document.getElementById('under-line')
 
 for (let i = 1; i < tabs.length; i++) {
     tabs[i].addEventListener("click", function(event){filter(event)});
 }
 
+for (let i = 0; i < games.length; i++) {
+    let game = {
+        id: randomIDGenerate(),
+        gameName: games[i].gameName,
+        gameImg: games[i].gameImg,
+        gameCategory: games[i].gameCategory
+    }
+
+    gameList.push(game);
+}
+
+// 화면 렌더링
 function render() {
     let resultHTML = ''
     let list = [];
 
     if (mode == 'all') {
-        list = taskList;
+        list = gameList;
     }
     else if (mode == 'ongoing' || mode == 'done') {
         list = filterList;
@@ -27,6 +40,7 @@ function render() {
     document.getElementById("task-board").innerHTML = resultHTML;
 }
 
+// 카테고리 별 렌더링
 function filter(event) {
     if (event) {
         mode = event.target.id;
@@ -40,19 +54,24 @@ function filter(event) {
         render();
     }
     else if (mode == 'ongoing') {
-        for (let i = 0; i < taskList.length; i++) {
-            if (taskList[i].isComplete == false) {
-                filterList.push(taskList[i])
+        for (let i = 0; i < gameList.length; i++) {
+            if (gameList[i].gameCategory == false) {
+                filterList.push(gameList[i])
             }
         }
     }
     else if (mode == 'done') {
-        for (let i = 0; i < taskList.length; i++) {
-            if (taskList[i].isComplete == true) {
-                filterList.push(taskList[i])
+        for (let i = 0; i < gameList.length; i++) {
+            if (gameList[i].gameCategory == true) {
+                filterList.push(gameList[i])
             }
         }
     }
 
     render();
+}
+
+// 랜덤 아이디 생성
+function randomIDGenerate() {
+    return '_' + Math.random().toString(36).substr(2, 9);
 }
